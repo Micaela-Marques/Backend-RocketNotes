d41132c2d51e71a53fba282347848d02593324fd
Micaela Marques <micaellagrangeiro@gmail.com>
2024-07-23 23:35:44 -0300

    increment pm2

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
+
 require('express-async-errors');
+require('dotenv').config()
 const migrationsRun = require('./database/sqlite/migrations');
 const AppError = require('./utils/AppError');
 const uploadConfig = require("./configs/upload");
@@ -7,6 +9,7 @@ const cors = require("cors")
 
 const express = require('express');
 const routes = require('./routes');
+const { config } = require('dotenv');
 
 migrationsRun()
 
@@ -34,5 +37,5 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3333
+const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
